3e5b522a8e4c0332d917fb594f85defb2fcdf260
anilarmour <anil.kumar09rocks@gmail.com>
2023-11-24 11:43:38 -0500

    Mysqlshell deploy

--- shell.sh
+++ shell.sh
@@ -8,4 +8,4 @@ systemctl start mysqld
 echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1';" > /tmp/errormes
 
 head=$(grep 'A temporary' /var/log/mysqld.log | awk '{print $11}')
-cat /tmp/errormes | mysql --connect-expired-password -uroot -p"{head}"
+cat /tmp/errormes | mysql --connect-expired-password -uroot -p"${head}"
