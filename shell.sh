4cdd063f4e26b1912f7c8b20f6737bd668ca6d42
anilarmour <anil.kumar09rocks@gmail.com>
2023-11-24 13:06:50 -0500

    Mysqlshell deploy

--- shell.sh
+++ shell.sh
@@ -3,8 +3,11 @@ dnf module disable mysql
 
 yum install mysql-community-server -y
 systemctl enable mysqld
-systemctl start mysqld
+systemctl restart mysqld
 
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1';" > /tmp/errormes
-head=$(grep 'A temporary' /var/log/mysqld.log | awk '{print $11}')
-cat /tmp/errormes | mysql --connect-expired-password -uroot -p"${head}"
+echo show databases | mysql -uroot -pRoboShop@1
+if [ $? -ne o ]
+then
+ echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1';" > /tmp/errormes
+ head=$(grep 'A temporary' /var/log/mysqld.log | awk '{print $11}')
+ cat /tmp/errormes | mysql --connect-expired-password -uroot -p"${head}"
