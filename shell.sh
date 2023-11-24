0baadafe25bc1a568ec82df51d25485129c43513
anilarmour <anil.kumar09rocks@gmail.com>
2023-11-24 13:38:47 -0500

    Mysqlshell deploy

--- shell.sh
+++ shell.sh
@@ -5,6 +5,10 @@ yum install mysql-community-server -y
 systemctl enable mysqld
 systemctl restart mysqld
 
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1';" > /tmp/errormes
+head=$(grep 'A temporary' /var/log/mysqld.log | awk '{print $11}')
+cat /tmp/errormes | mysql --connect-expired-password -uroot -p"${head}"
+
 echo show databases | mysql -uroot -pRoboShop@1
 if [ $? -ne o ]
 then
